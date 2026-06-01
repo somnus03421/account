f1a57b461a92d5d3d7bf6136d0bd00539f856c52
somnus03421 <154006379+somnus03421@users.noreply.github.com>
2026-06-02 04:05:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
-const CACHE = 'zhangben-v1';
-const FILES = ['./', './index.html', './manifest.json'];
+// 用當下時間當版本號
+const CACHE = 'zhangben-' + Date.now();
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
-);
-self.addEventListener('fetch', e =>
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(['./', './index.html', './manifest.json']))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e =>
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   )
+);
+
+self.addEventListener('fetch', e =>
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
 );
