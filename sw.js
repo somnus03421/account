3c9b92dbc3a0653202399686ce311eb28bf178af
Somnus <154006379+somnus03421@users.noreply.github.com>
2026-07-28 03:29:25 +0800

    Update service worker cache version and improve fetch handling

--- sw.js
+++ sw.js
@@ -1,22 +1,33 @@
-const CACHE = 'zhangben-v1008';
+const CACHE = 'zhangben-v1009';
 const FILES = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e =>
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE)
-      .then(c => c.addAll(FILES))
+      .then(cache => cache.addAll(FILES))
       .then(() => self.skipWaiting())
-  )
-);
+  );
+});
 
-self.addEventListener('activate', e =>
+
+self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(keys =>
+        Promise.all(
+          keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+        )
+      )
       .then(() => self.clients.claim())
-  )
-);
+  );
+});
 
-self.addEventListener('fetch', e =>
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
-);
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  );
+});
